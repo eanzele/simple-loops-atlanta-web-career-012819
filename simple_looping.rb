d66c6cfa4df4e53217855cbcd2f2aaa360bb1802
Emmanuel Anzele <eanzele@gmail.com>
2019-01-17 00:17:23 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,9 @@
 def loop_iterator(7)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
-  if 
+  if phrase < 8
+    break 
+  end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
