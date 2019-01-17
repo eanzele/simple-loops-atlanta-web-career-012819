ffc01d9f2bf0f256fb4a28aa4ddd045dc67ee971
Emmanuel Anzele <eanzele@gmail.com>
2019-01-17 00:16:01 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,8 @@
 
 def loop_iterator(7)
   loop do
-    
+    puts "Welcome to Flatiron School's Web Development Course!"
+  
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
