c485ba6d16932a459d3b797c64d12af2670796fb
Emmanuel Anzele <eanzele@gmail.com>
2019-01-17 00:27:31 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(7)
+def loop_iterator(number_of_times)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
   if phrase < 8
