f0a25fefb53fea98f2f3e1fb60db65a307b99e90
Emmanuel Anzele <eanzele@gmail.com>
2019-01-17 00:24:21 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -43,6 +43,7 @@ end
 end
 
 def for_iterator(7)
+  
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
